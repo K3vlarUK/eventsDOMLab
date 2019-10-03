1ff099422f83a7edbd738bd197ff62b141be3e97
Kevin McDermott <kmcdermott31@gmail.com>
2019-10-03 15:10:54 +0100

    MVP done

---
+++ lab_events_start/js/app.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const handleFormInput = function(event) {
+    event.preventDefault();
+    const newListItem = document.createElement('ul');
+    const newTitle = document.createElement('li')
+    newTitle.textContent = `${event.target.title.value}`;
+    const newAuthor = document.createElement('li');
+    newAuthor.textContent = `${event.target.author.value}`;
+    const newCategory = document.createElement('li');
+    newCategory.textContent = `${event.target.category.value}`;
+
+    const newReadingItem = document.querySelector('#reading-list')
+    newReadingItem.appendChild(newListItem);
+
+    newListItem.appendChild(newTitle);
+    newListItem.appendChild(newAuthor);
+    newListItem.appendChild(newCategory);
+    // const readingList = document.querySelector('#reading-list');
+    // readingList.appendChild
+    formInput.reset();
+  }
+
+  const formInput = document.querySelector('#new-item-form');
+  formInput.addEventListener('submit', handleFormInput);
+
+  console.log('JavaScript loaded');
+})
